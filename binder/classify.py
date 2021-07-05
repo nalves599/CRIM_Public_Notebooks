3844b9dda5525197eb0ae48e37907c5567a6b932
Richard Freedman <rfreedma@haverford.edu>
2021-07-05 09:00:13 -0400

    remove bug

--- binder/classify.py
+++ binder/classify.py
@@ -204,7 +204,6 @@ def batch_classify(
             index=["piece_title", "pattern_generating_match", "pattern_matched", "predicted_type", "sub_group_id"],
             columns="entry_number",
             values=["part", "start_offset", "start", "sum_durs"],
-            values=["part", "start_offset", "start", "sum_durs"],
             aggfunc=lambda x: x,
         )
         pivot_sort = pivot.sort_values(by=[("start_offset", 1)])
